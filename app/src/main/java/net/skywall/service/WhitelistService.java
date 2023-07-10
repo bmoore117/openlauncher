84f4e60f755b4851a24f02e9a3e26f0924a6b484
Benjamin Moore <moore.benjamin@live.com>
2023-07-10 10:17:55 -0400

    Add new google text to speech popup

--- app/src/main/java/net/skywall/service/WhitelistService.java
+++ app/src/main/java/net/skywall/service/WhitelistService.java
@@ -27,6 +27,7 @@ public class WhitelistService {
             "com.benny",
             "com.flask",
             "com.google.android.apps.inputmethod",
+            "com.google.android.apps.search.transcription",
             "com.google.android.apps.wellbeing",
             "com.google.android.documentsui",
             "com.google.android.gms",
